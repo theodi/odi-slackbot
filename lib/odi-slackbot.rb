a85bfde9716614cade9de6a15cbd713cc51db0d1
pezholio <pezholio@gmail.com>
2015-02-06 11:09:07 +0000

    Eval scripts should happen at startup
    Also: The path is wrong

--- lib/odi-slackbot.rb
+++ lib/odi-slackbot.rb
@@ -4,12 +4,13 @@ require 'sinatra'
 require_relative 'bot'
 
 @bot = OdiSlackbot::Bot.new
+@bot.eval_scripts(File.open(File.join("lib", "scripts", "*")))
 
 module OdiSlackbot
   class App < Sinatra::Base
 
     post '/' do
-      @bot.eval_scripts(File.open(File.join(".", "scripts", "*")))
+      @bot.handle_item(params)
     end
 
   end
