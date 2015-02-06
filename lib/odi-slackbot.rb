d99c7c0c19eeaf59fdfd4b5267413255aedd8a19
pezholio <pezholio@gmail.com>
2015-02-06 11:25:26 +0000

    Meh

--- lib/odi-slackbot.rb
+++ lib/odi-slackbot.rb
@@ -2,16 +2,22 @@ require 'slackbotsy'
 require 'sinatra'
 require 'yaml'
 
-config = YAML.load(ERB.new(File.read("config/config.yml")).result)
 
-@bot = Slackbotsy::Bot.new(config)
-@bot.eval_scripts(Dir[File.join(File.dirname(File.expand_path(__FILE__)), 'lib', 'scripts', '**', '*.rb')])
 
 module OdiSlackbot
   class App < Sinatra::Base
 
     post '/' do
-      @bot.handle_item(params)
+      bot.handle_item(params)
+    end
+
+    def bot
+      @bot ||= Slackbotsy::Bot.new(config)
+      @bot.eval_scripts(Dir[File.join(File.dirname(File.expand_path(__FILE__)), 'lib', 'scripts', '**', '*.rb')])
+    end
+
+    def config
+      YAML.load(ERB.new(File.read("config/config.yml")).result)
     end
 
   end
