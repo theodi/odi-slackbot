e5e005ac93659943120dcf9ae6fca5c3c2118099
pezholio <pezholio@gmail.com>
2015-02-06 11:34:12 +0000

    Geh

--- lib/scripts/dispense-crisps.rb
+++ lib/scripts/dispense-crisps.rb
@@ -1,8 +1,8 @@
-hear /dispense crisps/ do
-  num = rand(10)
-  if [2,7,9].include?(num)
-    HTTParty(:post "http://vending-machine.office.theodi.org/dispense?flavour=ready-salted")
-  else
+hear /.+ dispense crisps/i do
+  # num = rand(10)
+  # if [2,7,9].include?(num)
+  #   HTTParty(:post "http://vending-machine.office.theodi.org/dispense?flavour=ready-salted")
+  # else
     "Get your own damn crisps, yo"
-  end
+  #end
 end
